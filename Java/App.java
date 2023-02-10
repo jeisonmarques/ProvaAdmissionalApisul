2e2a184e765f863b812a6ae22828d024a94b52e6
Belarmino Nicolau Monteiro Simão <belarnicolau@hotmail.com>
2023-02-10 11:31:14 -0300

    apartment floor less used

--- Java/App.java
+++ Java/App.java
@@ -2,5 +2,10 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Running okay!");
+
+        System.out.println(String.join("\t","a. Qual é o andar menos utilizado pelos usuários"
+        // , 
+        // elevadorService.andarMenosUtilizado().toString()
+        ));
     }
 }
